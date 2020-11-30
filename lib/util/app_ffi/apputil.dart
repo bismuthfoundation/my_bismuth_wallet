4f523fb61ee6269907c1f5e14d4ae1f3b1540dab
redDwarf03 <rddwarf03@gmail.com>
2020-11-30 22:25:16 +0100

    kpub/kpriv/address generator

--- lib/util/app_ffi/apputil.dart
+++ lib/util/app_ffi/apputil.dart
@@ -1,5 +1,6 @@
 import 'package:bip32/bip32.dart' as bip32;
 import 'package:bip39/bip39.dart' as bip39;
+import 'package:bitcoin_flutter/bitcoin_flutter.dart';
 import 'package:ethereum_address/ethereum_address.dart';
 import 'package:flutter/material.dart';
 import 'package:hex/hex.dart';
@@ -11,42 +12,42 @@ import 'package:my_bismuth_wallet/service_locator.dart';
 import 'package:web3dart/web3dart.dart';
 
 class AppUtil {
+
+  String getAddress(node, [network]) {
+  return P2PKH(data: new PaymentData(pubkey: node.publicKey), network: network)
+      .data
+      .address;
+}
+
   Future<String> seedToAddress(String seed, int index) async {
     
     String mnemonic = bip39.entropyToMnemonic(seed);
-
+    print("Mnemonic : " + mnemonic);
     final bip39Seed = bip39.mnemonicToSeed(mnemonic);
+    print("BIP 39 Seed : " + HEX.encode(bip39Seed));
+
     final root = bip32.BIP32.fromSeed(bip39Seed);
+    print("BIP 32 Root Key : " + root.toBase58());
     bip32.BIP32 node = bip32.BIP32.fromBase58(root.toBase58());
     print("BIP 32 node (private Key) : " + HEX.encode(node.privateKey));
     print("BIP 32 node (public Key) : " + HEX.encode(node.publicKey));
     bip32.BIP32 child = node.derivePath("m/44'/209'/0'/0");
+    print("BIP 32 Extended private Key : " + child.toBase58());
+    bip32.BIP32 childNeutered = child.neutered();
+    print("BIP 32 Extended public Key : " + childNeutered.toBase58());
+
     print("BIP 32 child (private Key) : " + HEX.encode(child.privateKey));
     print("BIP 32 child (public Key) : " + HEX.encode(child.publicKey));
 
-    /*EthPrivateKey ethPrivateKey;
-    String addressEIP55;
-    // TODO: Distinct new address and regeneration of known address
-    for(int numAddress = 0; numAddress < 10; numAddress ++)
-   {
-      ethPrivateKey =
-          EthPrivateKey.fromHex(HEX.encode(child.derive(numAddress).privateKey));
-      final address0 = await ethPrivateKey.extractAddress();
-      addressEIP55 = checksumEthereumAddress(address0.toString());
-      //print("address EIP55 ("+numAddress.toString()+"): " + addressEIP55.toString());
-      AddressResponse addressResponse =
-        await AppService().getAddressResponse(addressEIP55);
-        if (addressResponse.result == null) {
-          break;
-        }
-    }*/
-     /*ethPrivateKey =
-          EthPrivateKey.fromHex(HEX.encode(child.derive(0).privateKey));
-      final address = await ethPrivateKey.extractAddress();
-      addressEIP55 = checksumEthereumAddress(address.toString());
-      //print("address EIP55 ("+numAddress.toString()+"): " + addressEIP55.toString());
-*/
-    return HEX.encode(child.publicKey);
+    bip32.BIP32 addressDerived0 = child.derive(0);
+    print("Public Key Derived Address (account 0) : " + HEX.encode(addressDerived0.publicKey));
+    print("Private Key Derived Address (account 0) : " + HEX.encode(addressDerived0.privateKey));
+    print("Private Key Wif : " + addressDerived0.toWIF());
+
+    String address = "0x4F545B" + getAddress(addressDerived0);
+    print("Address : " + address);
+
+    return address;
   }
 
   Future<void> loginAccount(String seed, BuildContext context) async {
