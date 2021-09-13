99c7e79a3d5b84472bff57d0ecc65deae58ee423
redDwarf03 <rddwarf03@gmail.com>
2021-09-13 19:03:13 +0200

    Updated german language file (Thx Damian)

--- lib/l10n/messages_de.dart
+++ lib/l10n/messages_de.dart
@@ -3,26 +3,24 @@
 // messages from the main program should be duplicated here with the same
 // function name.
 
-// @dart=2.9
-
 // Ignore issues from commonly used lints in this file.
-// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:unnecessary_brace_in_string_interps
 // ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
 // ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
-// ignore_for_file:unused_import, file_names
+// ignore_for_file:unused_import, file_names, always_declare_return_types
 
 import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
 
-final messages = new MessageLookup();
+final messages = MessageLookup();
 
-typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+typedef String MessageIfAbsent(String? messageStr, List<Object>? args);
 
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'de';
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static _notInlinedMessages(_) => <String, Function> {
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function> {
     "account" : MessageLookupByLibrary.simpleMessage("Konto"),
     "accounts" : MessageLookupByLibrary.simpleMessage("Konten"),
     "ackBackedUp" : MessageLookupByLibrary.simpleMessage("Bist du dir sicher, dass du deine Geheimsequenz oder deinen Seed gesichert hast?"),
@@ -66,7 +64,7 @@ class MessageLookup extends MessageLookupByLibrary {
     "createPasswordSecondParagraph" : MessageLookupByLibrary.simpleMessage("Das Passwort ist optional. Dein Wallet wird immer mithilfe einer PIN oder biometrischen Daten geschützt."),
     "createPasswordSheetHeader" : MessageLookupByLibrary.simpleMessage("Wählen"),
     "currency" : MessageLookupByLibrary.simpleMessage("Währung"),
-    "customUrlHeader" : MessageLookupByLibrary.simpleMessage("Custom Urls"),
+    "customUrlHeader" : MessageLookupByLibrary.simpleMessage("Definierte Urls"),
     "defaultAccountName" : MessageLookupByLibrary.simpleMessage("Hauptkonto"),
     "defaultNewAccountName" : MessageLookupByLibrary.simpleMessage("Name des neuen Kontos %1"),
     "diacritic" : MessageLookupByLibrary.simpleMessage("Gemeinsame Akzente und diakritische Zeichen werden durch ein gleichwertiges Zeichen ersetzt"),
@@ -76,18 +74,18 @@ class MessageLookup extends MessageLookupByLibrary {
     "encryptionFailedError" : MessageLookupByLibrary.simpleMessage("Wallet-Passwort konnte nicht festgelegt werden"),
     "enterAddress" : MessageLookupByLibrary.simpleMessage("Adresse eingeben"),
     "enterAmount" : MessageLookupByLibrary.simpleMessage("Betrag eingeben"),
-    "enterExplorerUrl" : MessageLookupByLibrary.simpleMessage("Enter a custom url explorer"),
-    "enterExplorerUrlInfo" : MessageLookupByLibrary.simpleMessage("(ex: https://bismuth.online/search?quicksearch=%1\n\'%1\' will be replaced by BIS address)"),
-    "enterExplorerUrlSwitch" : MessageLookupByLibrary.simpleMessage("Use a custom explorer url"),
+    "enterExplorerUrl" : MessageLookupByLibrary.simpleMessage("Trage eigene Explorer Url ein"),
+    "enterExplorerUrlInfo" : MessageLookupByLibrary.simpleMessage("(z.B. https://bismuth.online/search?quicksearch=%1\n\'%1\' wird durch BIS Adresse ersetzt)"),
+    "enterExplorerUrlSwitch" : MessageLookupByLibrary.simpleMessage("Benutze eigene Explorer Url"),
     "enterOpenfield" : MessageLookupByLibrary.simpleMessage("Data (Openfield) eingeben"),
     "enterOperation" : MessageLookupByLibrary.simpleMessage("Enter Operation"),
     "enterPasswordHint" : MessageLookupByLibrary.simpleMessage("Passwort eingeben"),
-    "enterTokenApi" : MessageLookupByLibrary.simpleMessage("Enter url token api"),
+    "enterTokenApi" : MessageLookupByLibrary.simpleMessage("Trage URL Token API ein"),
     "enterTokenApiInfo" : MessageLookupByLibrary.simpleMessage("(ex: https://bismuth.today/api/balances/)"),
     "enterTokenQuantity" : MessageLookupByLibrary.simpleMessage("Anzahl Eingeben"),
-    "enterWalletServer" : MessageLookupByLibrary.simpleMessage("Enter a custom wallet server"),
+    "enterWalletServer" : MessageLookupByLibrary.simpleMessage("Trage eigenen Wallet Server ein"),
     "enterWalletServerInfo" : MessageLookupByLibrary.simpleMessage("ip:port (ex: 11.22.33.44:2000)"),
-    "enterWalletServerSwitch" : MessageLookupByLibrary.simpleMessage("Use a custom wallet server"),
+    "enterWalletServerSwitch" : MessageLookupByLibrary.simpleMessage("Benutze eignen Wallet Server"),
     "exampleCardFrom" : MessageLookupByLibrary.simpleMessage("Von jemandem"),
     "exampleCardIntro" : MessageLookupByLibrary.simpleMessage("Willkommen bei my Bismuth Wallet. Wenn du Bismuth sendest oder empfängst, sieht es aus wie folgt: "),
     "exampleCardLittle" : MessageLookupByLibrary.simpleMessage("Ein paar"),
@@ -123,7 +121,7 @@ class MessageLookup extends MessageLookupByLibrary {
     "logoutDetail" : MessageLookupByLibrary.simpleMessage("Beim Ausloggen werden dein Seed und alle mit my Bismuth Wallet verbundenen Daten von diesem Gerät gelöscht. Falls du deinen Seed nicht gesichert hast, verlierst du dauerhaft den Zugriff auf dein Guthaben."),
     "logoutReassurance" : MessageLookupByLibrary.simpleMessage("Solange du deinen Seed gesichert hast, musst du dir keine Gedanken machen."),
     "manage" : MessageLookupByLibrary.simpleMessage("Verwaltung"),
-    "mempool" : MessageLookupByLibrary.simpleMessage("Unconfirmed"),
+    "mempool" : MessageLookupByLibrary.simpleMessage("Unbestätigt"),
     "mnemonicInvalidWord" : MessageLookupByLibrary.simpleMessage("%1 ist kein gültiges Wort"),
     "mnemonicSizeError" : MessageLookupByLibrary.simpleMessage("Die Geheimsequenz muss 24 Wörter enthalten"),
     "myTokens" : MessageLookupByLibrary.simpleMessage("Tokens"),
@@ -162,7 +160,7 @@ class MessageLookup extends MessageLookupByLibrary {
     "qrUnknownError" : MessageLookupByLibrary.simpleMessage("QR-Code konnte nicht gelesen werden"),
     "receive" : MessageLookupByLibrary.simpleMessage("Empfangen"),
     "received" : MessageLookupByLibrary.simpleMessage("Empfangen"),
-    "releaseNoteHeader" : MessageLookupByLibrary.simpleMessage("What\'s new"),
+    "releaseNoteHeader" : MessageLookupByLibrary.simpleMessage("Neuigkeiten"),
     "removeAccountText" : MessageLookupByLibrary.simpleMessage("Bist du dir sicher, dass du dieses Konto verbergen willst? Du kannst es später durch Tippen auf den \"%1\"-Button wieder hinzufügen."),
     "removeContact" : MessageLookupByLibrary.simpleMessage("Kontakt löschen"),
     "removeContactConfirmation" : MessageLookupByLibrary.simpleMessage("Willst du %1 wirklich löschen?"),
